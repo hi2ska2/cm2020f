c5b5abc7cb088f8f0847f678ff7551d8ad2662d7
JuyeongPark <acejyp97@gist.ac.kr>
2020-09-13 21:20:44 +0900

    Create HW03_1.m

---
+++ s20194005/HW03/HW03_1.m
@@ -0,0 +1,22 @@
+prompt = 'Choose the number of N ';
+N= input(prompt);
+
+Array = zeros(N,N); % Matrix declaration
+
+for i=2:N-1;
+    Array(i,i-1) = 1; 
+    Array(i,i) = -2; 
+    Array(i,i+1) = 1;
+end
+
+Array(1,1)=1;
+Array(N,N)=1;
+
+b=zeros(N,1);
+b(1,1)=1;
+b(N,1)=0;
+
+x=0:1/(N-1):1;
+y=inv(Array)*b;
+
+plot(x,y);
